6dfca2fd32cd16af3d50a35fa7a6a6495dc3a831
fantastic-scope-1514 <dframe22@gmail.com>
2020-03-22 05:22:03 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,21 +2,10 @@
 first_number = 5.0
 second_number = 4.0
 
-def sum
-  first_number + second_number
-end
-
-def difference
-  first_number - second_number
-end
-
-def product
-  first_number * second_number
-end
-
-def quotient
-  first_number / second_number
-end
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
 
 sum
 difference
