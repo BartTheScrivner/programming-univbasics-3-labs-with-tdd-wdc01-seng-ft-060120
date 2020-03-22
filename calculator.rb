fbf41877d02b9c087e66f1aa19ddc6b8a0588d94
fantastic-scope-1514 <dframe22@gmail.com>
2020-03-22 05:18:54 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,24 @@
 # Add your variables here
+first_number = 5.0
+second_number = 4.0
+
+def sum
+  first_number + second_number
+end
+
+def difference
+  first_number - second_number
+end
+
+def product
+  first_number * second_number
+end
+
+def quotient
+  first_number / second_number
+end
+
+sum
+difference
+product
+quotient
